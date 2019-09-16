9d62838e037344beee720896ed9c25497f19391b
mohamed aboelnour <mohamed2030aboelnour@gmail.com>
2019-09-16 23:21:27 +0200

    first commit

---
+++ test2.js
@@ -0,0 +1,29 @@
+$(function(){
+    
+    
+    $(".ani").animate({
+       top:'15px',
+       height:'6px' 
+    },1000);
+        
+    $(".ani").animate({
+       width:'400px' 
+    },1000);
+        
+    $(".ani").animate({
+       height:'600px',
+       borderRadius:'15px'
+    },1000);
+
+    $('.d2').animate({
+      fontSize:'40px'
+    },2000)
+    $('.d2').fadeIn(2000);
+
+    
+
+          
+    
+    
+    
+})();
